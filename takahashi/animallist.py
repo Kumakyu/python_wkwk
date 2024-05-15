227533f1261ed11094943d2bf8f687c6f39d6ee0
Kumakyu <takahashi.yusuke.ma@mynavi.jp>
2024-05-15 00:40:51 +0000

    ad  file animallist.py

--- takahashi/animallist.py
+++ takahashi/animallist.py
@@ -3,8 +3,11 @@
 animallist = ["giraffe","tiger", "zebra","elephant", "lion"]
 #1番の問題
 animallist[2] = animallist[3]
+print(animallist, end="")
 #2番の問題
 del animallist[4]
+print(animallist, end="")
 #3番の問題
-
+list_asc = animallist.sort()
+print(animallist, end="")
 
