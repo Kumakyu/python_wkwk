deced08ea33eb5ab6578d43c090206466cc549e5
Kumakyu <takahashi.yusuke.ma@mynavi.jp>
2024-05-15 05:36:07 +0000

    edit file 'population.py'

--- takahashi/population.py
+++ takahashi/population.py
@@ -5,9 +5,8 @@ args = sys.argv
 #順位の入力
 rank = int(args[1])
 #リストの定義
-countrylist = ("China","India","U.S.A","Indonesia","Pakistan","Brazil","Nigeria","Bangladesh","Russia","Mexico")
+country_tuple = ("China","India","U.S.A","Indonesia",
+               "Pakistan","Brazil","Nigeria","Bangladesh","Russia","Mexico")
 #順位の出力
-contry_rank = countrylist[rank]
+contry_rank = country_tuple[rank -1]
 print(contry_rank)
-
-
