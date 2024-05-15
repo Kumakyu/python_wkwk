2e7ee86de9f08c70ea18cfa613eab880e391224a
Kumakyu <takahashi.yusuke.ma@mynavi.jp>
2024-05-15 02:30:57 +0000

    edit 'animallist.py'

--- takahashi/animallist.py
+++ takahashi/animallist.py
@@ -1,13 +1,20 @@
 # 動物の名前のリストのプログラム
+#引数のためのモジュールをインポート
+import sys
+args = sys.argv
 # リストの作成
-animallist = ["giraffe","tiger", "zebra","elephant", "lion"]
+animallist = ["giraffe","tiger","zebra","elephant", "lion"]
 #1番の問題
-animallist[2] = animallist[3]
+#引数の入力
+num = int(args[1])
+name = args[2]
+#動物名の挿入
+animallist[num] = name
 print(animallist, end="")
 #2番の問題
-del animallist[4]
-print(animallist, end="")
-#3番の問題
-list_asc = animallist.sort()
-print(animallist, end="")
+# del animallist[4]
+# print(animallist, end="")
+# #3番の問題
+# list_asc = animallist.sort()
+# print(animallist, end="")
 
