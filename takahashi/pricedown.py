68f2f35f38de221c490f0508b024eeacf4210057
Kumakyu <takahashi.yusuke.ma@mynavi.jp>
2024-05-17 04:01:40 +0000

    add file pricedown.py

---
+++ takahashi/pricedown.py
@@ -0,0 +1,29 @@
+import sys
+args = sys.argv
+
+#引数を変数に代入
+hm_class = args[1]              #値下げ対象の種別
+price_down = int(args[2])   #値下げ額
+
+#品目（品名と価格）を辞書型で定義
+hinmoku = {"リンゴ":80 , "みかん":198, "バナナ":150, 
+           "ビール":360, "日本酒":580, 
+           "ラーメン":380, "うどん":128, "パスタ":258}
+
+#区分ごとの定義
+fruits = ("リンゴ", "みかん", "バナナ")            #果物類をタプルで定義
+alcohol = ("ビール", "日本酒")                         #酒類をタプルで定義
+noodles = ("ラーメン", "うどん", "パスタ")   #麺類をタプルで定義
+
+if hm_class == "果物類":
+    for item in fruits:
+        hinmoku[item] = max(hinmoku[item] - price_down, 1)
+    print(hinmoku,end="")
+elif hm_class == "酒類":
+    for item in alcohol:
+        hinmoku[item] = max(hinmoku[item] - price_down, 1)
+    print(hinmoku,end="")
+elif hm_class == "麺類":
+    for item in noodles:
+        hinmoku[item] = max(hinmoku[item] - price_down, 1)
+    print(hinmoku,end="")
