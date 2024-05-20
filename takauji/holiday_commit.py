923e9f0597e5010efcf532cd3228f6218e8b2c6f
takaujiji <takauji.tomone.qv@mynavi.jp>
2024-05-20 07:58:33 +0000

    演習問題を追加

---
+++ takauji/holiday_commit.py
@@ -0,0 +1,10 @@
+from datetime import date
+from database import session
+from tables import Holiday
+ 
+ 
+# データを取得する
+holidaylist=session.query(Holiday).all()
+ 
+for holiday in holidaylist:
+    print(holiday.holi_date,holiday.holi_text)
