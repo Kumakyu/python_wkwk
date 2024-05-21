8d362f6ecbe810af683f564a36dddf5ef8bcafae
Kumakyu <takahashi.yusuke.ma@mynavi.jp>
2024-05-21 06:00:41 +0000

    add file

---
+++ takahashi/aquaattend_table.py
@@ -0,0 +1,22 @@
+# テーブルの定義
+import sys
+from sqlalchemy import Column, String, Date, Integer, Numeric, DateTime
+from database import Base
+from database import ENGINE
+
+# テーブル:attendnumの定義
+class Attendnum(Base):
+    __tabelname__ = "attendnum"
+    entry_date = Column('entry_name', Date, primary_key = True)
+    seq = Column('seq', Integer, primary_key = True)
+    adult = Column('adult', Integer)
+    child = Column('child', Integer)
+
+def main(args):
+    """
+    メイン関数
+    """
+    Base.metadata.create_all(bind=ENGINE)
+
+if __name__ == "__main__":
+        main(sys.argv)
